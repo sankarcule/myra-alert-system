c53ca7fabff42972c0c7ad81dad790be8cc37f68
Sankar <sankarcule@gmail.com>
2017-12-19 17:28:10 +0530

    api_docs - swagger

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,16 +1,28 @@
 class AlertsController < ApplicationController
   before_action :set_alert, only: [:destroy]
 
+  # Returns the list of alerts
+  #
+  # @response_class Array<AlertSerializer>
   def index
     @alerts = Alert.all
     json_response(@alerts)
   end
 
+  # Create Alert
+  #
+  # @query_parameter [integer] reference_id
+  # @query_parameter [integer] delay
+  # @query_parameter [string] description
+  #
   def create
     @alert = Alert.create!(alert_params)
     json_response(@alert, :created)
   end
 
+  # Delete Alert
+  #
+  #
   def destroy
     @alert.destroy
     head :no_content
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :alerts, only: [:index, :create, :destroy]
+
+  mount Swaggard::Engine, at: '/api_docs/swagger/'
 end
